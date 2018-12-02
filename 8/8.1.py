a142046d15ad32846f7ee676e8bb5086de15dc6a
KetchupOnWaffles <44327873+KetchupOnWaffles@users.noreply.github.com>
2018-12-02 10:50:47 +0100

    Update 8.1.py

--- 8/8.1.py
+++ 8/8.1.py
@@ -1,8 +1,8 @@
-    s = [5,9,1,2,7,0]
+def tri_selection(s):
     longueur = len(s)
     for i in range(longueur):
         j = i+1
         for j in range(longueur-1):
             if s[i] < s[j]:
                 s[i],s[j] = s[j],s[i]
-print(alist)
+    return s
